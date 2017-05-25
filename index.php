333be42b81fa7ca789509e022eab20b1a4febb91
cyrusliew <cyrusliew02@gmail.com>
2017-05-25 21:10:47 +1200

    Site setup

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<title>Cyrus Liew</title>
+	<link rel="icon" type="image/x-icon" href="images/favicon-new.png">
+	<link rel="stylesheet" type="text/css" href="css/tether.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.fullpage.min.css">
+	<!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i,800i" rel="stylesheet"> -->
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/slick.css">
+	<link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/mainstyles.css">
+
+	<script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
+	<script type="text/javascript" src="js/tether.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery.fullpage.min.js"></script>
+	<script type="text/javascript" src="js/TweenMax.min.js"></script>
+	<script type="text/javascript" src="js/slick.min.js"></script>
+	<script type="text/javascript" src="js/mainjs.js"></script>
+</head>
+<body class="loading">
+	<div class="cloader"></div>
+	<button class="nav"><i class="fa fa-navicon"></i></button>
+	<nav id="menu">
+		<li data-menuanchor="intro"><a href="#intro">intro</a></li>
+		<li data-menuanchor="about"><a href="#about">about</a></li>
+		<li data-menuanchor="past-and-present"><a href="#past-and-present">past &amp; present</a></li>
+		<li data-menuanchor="what-is-inside"><a href="#what-is-inside">what is inside</a></li>
+		<li data-menuanchor="creation"><a href="#creation">creation</a></li>
+	</nav>
+	<div id="main-container" class="container-fluid">
+	    <div id="intro" name="Intro" class="section active">
+	    	&nbsp;
+	    </div>
+	    <div id="about" name="About" class="section">
+		    <div class="container">
+		    	<div class="left-info col-xs-12 col-sm-6">
+		    		<p class="title">name</p>
+					<p>Cyrus Liew</p>
+					<p class="title">email</p>
+					<p>cyrusliew02@gmail.com</p>
+					<p class="title">portfolio</p>
+					<p><a href="http://www.behance.net/cyrus_liew" target="_blank">www.behance.net/cyrus_liew</a></p>
+		    	</div>
+		    	<div class="right-intro col-xs-12 col-sm-6">
+		    		<p>Cyrus Liew my name, a Malaysia based multimedia designer. I am passionate about my life, always have fun in getting the biggest satisfaction when working on any design works. Love about being an inspiration of people around. With 2 years of working experience after getting my diploma, I am now diving into the world of UX Design, while expanding the knowledge in Web Design &amp; Development.</p>
+		    	</div>
+		    </div>
+	    </div>
+	    <div id="past-and-present" name="Past &amp; Present" class="section">
+	    	<div class="container">
+		    	<div class="pnp-content col-xs-12 col-sm-7">
+		    		<h4 class="timeline">DEC 2012</h4>
+		    		<h3 class="company">Big Bad Wolf Book Sale Malaysia</h3>
+		    		<h3 class="role">as Floor Crew</h3>
+		    		<p class="desc">I was part of the floor crew under children book section area, giving the Big Bad Wolf Book Sale the largest book sale in the world, I have developed myself to be able to cope with Mount Everest sized of messy books.</p>
+		    	</div>
+		    	<div class="margin col-sm-1"></div>
+		    	<div class="acquirement col-xs-12 col-sm-4">
+		    		<h4 class="title">ACQUIRED SKILLS</h4>
+		    		<div class="badges">
+		    			<div><img src="images/badges/organizing-lvl-1.png" width="142" height="auto" /></div>
+		    			<div><img src="images/badges/patient-lvl-1.png" width="142" height="auto" /></div>
+		    		</div>
+		    	</div>
+	    	</div>
+	    </div>
+	    <div id="what-is-inside" name="What is Inside" class="section">
+	    	<div class="container wii-badges">
+	    		<div class="first-row">
+		    		<img src="images/badges/communication-lvl-4.png" alt="Communication Skill Level 4">
+		    		<img src="images/badges/apple-lvl-3.png" alt="Apple Knowledge 3">
+		    		<img src="images/badges/organizing-lvl-3.png" alt="Organizing Skill Level 3">
+	    		</div>
+	    		<div class="second-row">
+		    		<img src="images/badges/cooking-lvl-2.png" alt="Cooking Skill Level 2">
+		    		<img src="images/badges/patient-lvl-2.png" alt="Communication Skill Level 2">
+		    		<img src="images/badges/leadership-lvl-beta.png" alt="Leadership Skill Level Beta">
+	    		</div>
+	    	</div>
+	    </div>
+	    <div id="creation" name="Creation" class="section">
+		    <div class="container">
+		    	<div class="col-xs-12 col-sm-4 col-md-3 project">
+		    		<div class="holder">
+		    			<img class="screenshot" src="images/creation/wtsc.png" />
+		    			<a href="#">
+		    				<span class="middle">
+			    				<i class="fa fa-graduation-cap"></i>
+			    				<h3 class="title">why are they so cold</h3>
+			    				<p class="date">NOV 2014</p>
+		    				</span>
+		    			</a>
+		    		</div>
+		    	</div>
+		    </div>
+	    </div>
+	    <div id="get" name="Get" class="section">
+		    <div class="container">
+		    	<div class="col-sm-5">
+		    		<form>
+		    			<input class="form-control" type="email" name="email" placeholder="Email">
+		    			<input class="form-control" type="text" name="subject" placeholder="Subject">
+		    			<textarea class="form-control" name="message" placeholder="Message"></textarea>
+		    		</form>
+		    	</div>
+		    	<div class="col-sm-1"></div>
+		    	<div class="col-sm-6">
+		    		<div class="social-links">
+		    			<a href="http://www.instagram.com/cyrus_liew" target="_blank"><i class="fa fa-instagram"></i></a>
+		    			<a href="http://www.behance.net/cyrus_liew" target="_blank"><i class="fa fa-behance"></i></a>
+		    			<a href="tel:+64277712177"><i class="fa fa-phone"></i></a>
+		    		</div>
+		    		<div class="current-status">
+		    			<p>Cyrus Liew is currently on working holiday in Auckland Central, New Zealand, and looking forward to get work visa in order to jump into UX/ Web Design/ Development in this beautiful country.<br><br>
+		    			Available for freelance project online, long term position in New Zealand.</p>
+		    		</div>
+		    	</div>
+		    </div>
+	    </div>
+	</div>
+	<div id="csstar-fam">
+		<svg class="csstarsvg">
+			<defs>
+				<linearGradient id="fadeLine1" x1="0%" x2="100%" y1="0%" y2="100%">
+					<stop offset="0" style="stop-color:#ffc700;stop-opacity:0;" />
+					<stop offset="0.55" style="stop-color:#ffc700;" />
+					<stop offset="0.8" style="stop-color:#ffc700;" />
+					<stop offset="1" style="stop-color:#ffc700;stop-opacity:0;" />
+				</linearGradient>
+				<linearGradient id="fadeLine2" x1="0%" x2="100%" y1="0%" y2="100%">
+					<stop offset="0" style="stop-color:#ffc700;stop-opacity:0;" />
+					<stop offset="0.2" style="stop-color:#ffc700;" />
+					<stop offset="0.5" style="stop-color:#ffc700;" />
+					<stop offset="1" style="stop-color:#ffc700;stop-opacity:0;" />
+				</linearGradient>
+				<linearGradient id="fadeLine3" x1="0%" x2="100%" y1="0%" y2="100%">
+					<stop offset="0" style="stop-color:#ffc700;stop-opacity:0;" />
+					<stop offset="0.55" style="stop-color:#ffc700;" />
+					<stop offset="
+					0.7" style="stop-color:#ffc700;" />
+					<stop offset="1" style="stop-color:#ffc700;stop-opacity:0;" />
+				</linearGradient>
+				<linearGradient id="fadeLine4" x1="0%" x2="100%" y1="0%" y2="100%">
+					<stop offset="0" style="stop-color:#ffc700;stop-opacity:0;" />
+					<stop offset="0.55" style="stop-color:#ffc700;" />
+					<stop offset="0.8" style="stop-color:#ffc700;stop-opacity:0" />
+				</linearGradient>
+			</defs>
+			<g>
+				<line x1="27.18%" y1="21.54%" x2="100%" y2="95.71%" stroke="url(#fadeLine4)" />
+				<line x1="0" y1="0" x2="40.8%" y2="40.8%" stroke="url(#fadeLine1)" />
+				<line x1="38.54%" y1="42.26%" x2="95.19%" y2="100%" stroke="url(#fadeLine3)" />
+				<line x1="13.77%" y1="22.6%"" x2="89.72%" y2="100%" stroke="url(#fadeLine2)" />
+				<rect x="35%" y="24.78%" height="14" width="14" />
+				<rect x="46.18%" y="36.1%" height="12" width="12" />
+				<rect x="34%" y="37.43%" height="8" width="8" />
+			</g>
+		</svg>
+	</div>
+	<div id="ccircle-fam">
+		<svg class="ccirclesvg">
+			<defs>
+				<linearGradient id="fadeGrad" x1="40%" x2="43%" y1="40%" y2="55%">
+					<stop offset="0" style="stop-color:#fff;" />
+					<stop offset="100" style="stop-color:#000;" />
+				</linearGradient>
+				<mask id="fade" maskUnits="userSpaceOnUse">
+					<rect width="100%" height="100%" fill="url(#fadeGrad)" />
+				</mask>
+			</defs>
+			<g id="ccircle-group">
+				<g id="ccircle-outer" mask="url(#fade)">
+					<circle id="XMLID_39_" class="ccircle" cx="50%" cy="50%" r="76"/>
+					<circle id="XMLID_42_" class="ccircle" cx="50%" cy="50%" r="134.5"/>
+					<circle id="XMLID_55_" class="ccircle" cx="50%" cy="50%" r="238.3"/>
+					<circle id="XMLID_56_" class="ccircle" cx="50%" cy="50%" r="422.1"/>
+					<!-- <circle id="XMLID_57_" class="ccircle" cx="50%" cy="50%" r="50%"/> -->
+				</g>
+				<!-- <g id="ccircle-main">
+					<circle id="ccircle-main" class="ccircle" cx="50%" cy="50%" r="62.7"/>
+				</g> -->
+			</g>
+		</svg>
+		<div id="ccircle-pic">
+			<div class="cimg-holder"><img width="42" height="auto" src="images/logo.png"></div>
+			<div class="cyear">
+				<span class="year">2012</span>
+				<div class=" yearnav">
+					<button class="arw-left">
+						<svg viewbox="0 0 100 100">
+							<path d="M50 0 L0 50 L50 100 Z" />
+						</svg>
+					</button>
+					<button class="arw-right active">
+						<svg viewbox="0 0 100 100">
+							<path d="M50 0 L100 50 L50 100 Z" />
+						</svg>
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="csite-name">
+			<div class="csite-name-text">
+				<h1 class="csite-heading">&#123; cyrus liew &#125;</h1>
+				<p class="csite-pos-desc">Graphic Design /<br>
+				<span class="ctab-space">&emsp; &emsp;</span> Web Design &amp; Development /<br>
+				<span class="ctab-space">&emsp; &emsp; &emsp; &emsp;</span> Front-End Development</p>
+				<!-- <p class="csite-pos-desc">平面设计 /<br>
+				<span class="ctab-space">&emsp; &emsp;</span> 网页设计及开发 /<br>
+				<span class="ctab-space">&emsp; &emsp; &emsp; &emsp;</span> 前端开发</p> -->
+			</div>
+		</div>
+	</div>
+	<div class="scroll-hint">
+		<div class="scroller"></div>
+		<div class="mouse"></div>
+	</div>
+	<div id="bg-mt-cook">
+		<img width="1932" height="auto" src="images/mt-cook.png">
+	</div>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$("body").removeClass("loading");
+			$(".badges").slick({
+				slidesToShow: 2,
+				infinite: false
+			});
+		})
+	</script>
+</body>
+</html>
